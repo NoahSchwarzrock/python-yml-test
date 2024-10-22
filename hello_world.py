9f98da9659392753c061c41190a750a4b16303cf
Schwarzrock, Noah (SE-A/32) <noah.schwarzrock@volkswagen.de>
2024-10-22 10:06:42 +0200

    fix

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ def new_func(name):
     return (f"Hallo {name}")
 
 class TestClass(unittest.TestCase):
-    def greetingsTest1(self):
+    def test_greetings1(self):
         self.assertEqual(new_func("Noah"), "Hallo Noah")
 
 if __name__ == '__main__':
